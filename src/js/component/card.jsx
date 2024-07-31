34bb7947d2a14b134de3ebd6384e1f84d63fcd18
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-07-31 22:47:22 +0000

    in progress

---
+++ src/js/component/card.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export function Card()
+{
+    return 
+    (<div>
+        <img/>
+        <div className="middiv">
+        <div className="title"></div>
+        <div className="text"></div>
+        </div>
+        <div className="footdiv"><button></button></div>
+    </div>);
+}
