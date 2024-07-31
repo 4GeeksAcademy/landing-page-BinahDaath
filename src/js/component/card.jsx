398e57cd452eb4be44dc6caa46d71d2d49ee822b
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-07-31 23:51:42 +0000

    in progress

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 
 export function Card()
 {
-    return 
-    (<div>
-        <img/>
-        <div className="middiv">
-        <div className="title"></div>
-        <div className="text"></div>
+    return (
+        <div className="flex flex-col"  style={{"width": "300px"}}>
+            <img src="https://placehold.co/500x325" className="w-full object-fill h-4"/>
+            <div className="middiv flex flex-col justify-items-center">
+            <div className="title m-0">title</div>
+            <div className="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sapiente tempore facere quam consectetur repudiandae accusamus velit nulla nam, maxime quidem quia odio blanditiis, consequatur dolor molestiae pariatur beatae sequi.</div>
+            </div>
+            <div className="footdiv"><button>button</button></div>
         </div>
-        <div className="footdiv"><button></button></div>
-    </div>);
+        );
 }
