c22ce22fc7d4bd6026878c1a10daf815709d2841
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-07-31 23:53:42 +0000

    in progress

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -4,7 +4,7 @@ export function Card()
 {
     return (
         <div className="flex flex-col"  style={{"width": "300px"}}>
-            <img src="https://placehold.co/500x325" className="w-full object-fill h-4"/>
+            <img src="https://placehold.co/500x325" className="w-full"/>
             <div className="middiv flex flex-col justify-items-center">
             <div className="title m-0">title</div>
             <div className="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sapiente tempore facere quam consectetur repudiandae accusamus velit nulla nam, maxime quidem quia odio blanditiis, consequatur dolor molestiae pariatur beatae sequi.</div>
