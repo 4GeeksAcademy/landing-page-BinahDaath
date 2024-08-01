1a1e564772c8826de5f1a2b316ba9a7d125257ef
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-01 00:19:14 +0000

    in progress

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 export function Card({title,text})
 {
     return (
-        <div className="flex flex-col justify-center"  style={{"width": "300px"}}>
+        <div className="flex flex-col justify-center bg-slate-50"  style={{"width": "300px"}}>
             <img src="https://placehold.co/500x325" className="w-full"/>
-            <div className="middiv flex flex-col gap-y-5">
+            <div className="middiv flex flex-col gap-y-5 bg-white">
             <div className="title mx-auto">{title}</div>
             <div className="text mx-auto">{text}</div>
             </div>
