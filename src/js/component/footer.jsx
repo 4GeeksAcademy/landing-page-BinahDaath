2070dc8bbc021cd676d943f5ad9092e036ddcf87
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-01 05:10:48 +0000

    done

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export function Footer()
+{
+    return (
+        <div className="flex flex-col bg-black" >
+        </div>
+        );
+}
--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,7 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 import {Card} from "./component/card.jsx";
 import {Header} from "./component/header.jsx";
+import {Footer} from "./component/footer.jsx";
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<div><Header/><Card title="test" text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sapiente tempore facere quam consectetur repudiandae accusamus velit nulla nam, maxime quidem quia odio blanditiis, consequatur dolor molestiae pariatur beatae sequi."/></div>);
+ReactDOM.createRoot(document.getElementById('app')).render(<div><Header/><Card title="test" text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sapiente tempore facere quam consectetur repudiandae accusamus velit nulla nam, maxime quidem quia odio blanditiis, consequatur dolor molestiae pariatur beatae sequi."/><Footer/></div>);
 
