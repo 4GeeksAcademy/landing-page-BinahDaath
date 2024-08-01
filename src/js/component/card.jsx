b0225536053851a84f462001b2858a7b9bf3bbbe
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-01 06:03:38 +0000

    in progress

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export function Card({title,text})
 {
     return (
-        <div className="flex flex-col justify-center bg-slate-50 mb-3"  style={{"width": "300px"}}>
+        <div className="flex flex-col justify-center bg-slate-50 mb-3 border-1 border-black"  style={{"width": "300px"}}>
             <img src="https://placehold.co/500x325" className="w-full"/>
             <div className="middiv flex flex-col gap-y-5 bg-white">
             <div className="title mx-auto text-xl">{title}</div>
--- src/js/component/header.jsx
+++ src/js/component/header.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export function Header()
 {
     return (
-        <div className="flex flex-col bg-slate-50" >
+        <div className="flex flex-col bg-slate-50 mb-3" >
             <div className="title text-5xl">A Warm Welcome!</div>
             <div className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur aperiam, ratione ducimus, velit alias accusantium corrupti vel animi accusamus distinctio architecto assumenda inventore aliquam, rem corporis sunt. Vel, sapiente dolorum?</div>
             <div><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2">button</button></div>
