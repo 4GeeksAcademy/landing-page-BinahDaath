ecbaa81c42d9d073904ece1fd5cf4aeac4cea8b1
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-01 05:05:12 +0000

    in progress

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 import {Card} from "./component/card.jsx";
+import {Header} from "./component/header.jsx";
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Card title="test" text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sapiente tempore facere quam consectetur repudiandae accusamus velit nulla nam, maxime quidem quia odio blanditiis, consequatur dolor molestiae pariatur beatae sequi."/>);
+ReactDOM.createRoot(document.getElementById('app')).render(<div><Header/><Card title="test" text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa sapiente tempore facere quam consectetur repudiandae accusamus velit nulla nam, maxime quidem quia odio blanditiis, consequatur dolor molestiae pariatur beatae sequi."/></div>);
 
