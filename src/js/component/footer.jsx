567c1f2e3e5e5f55d9a28fe153a693f1d81c715a
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-01 05:40:19 +0000

    in progress

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export function Footer()
 {
     return (
-        <div className="bg-black text-white" style={{height:"30px"}}>
+        <div className="flex flex-row w-full bg-black text-white fixed bottom-0" style={{height:"30px"}}>
             <div className="mx-auto">texttext</div>
         </div>
         );
